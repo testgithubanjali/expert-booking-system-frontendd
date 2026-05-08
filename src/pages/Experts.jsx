cf4342f7f1827b19fa794fd0c574010a6024e0b3
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-05-09 01:38:46 +0530

    add: added code inside Loader

---
+++ src/pages/Experts.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { API } from "../api/api";
+import ExpertCard from "../components/ExpertCard";
+
+function Experts() {
+  const [experts, setExperts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const fetchExperts = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await API.get(
+        `/experts?page=${page}&search=${search}&category=${category}`
+      );
+
+      setExperts(data.experts);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(fa
